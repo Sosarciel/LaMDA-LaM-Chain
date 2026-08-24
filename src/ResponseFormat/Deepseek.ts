3eb1519a40599eafdfde5a607c846c5193b8702c
zwa73 <ssfen2419@126.com>
2026-08-25 00:26:13 +0800

    add DeepseekTextResponseExample

--- src/ResponseFormat/Deepseek.ts
+++ src/ResponseFormat/Deepseek.ts
@@ -146,3 +146,33 @@ export const DeepseekChatResponseExample = {
     },
     system_fingerprint: "fp_8333852bec_prod0820_fp8_kvcache",
 } satisfies DeepseekChatResponse;
+
+export const DeepseekTextResponseExample = {
+    id: "456a034b-6e31-4a4d-9548-e87b5d694ae0",
+    object: "text_completion",
+    created: 1759123711,
+    model: "deepseek-v4-pro",
+    choices: [
+        {
+            finish_reason: "stop",
+            index: 0,
+            logprobs: null,
+            text: "您好，有什么需要帮助的吗？",
+        },
+        {
+            finish_reason: "stop",
+            index: 1,
+            logprobs: null,
+            text: "您好，有什么需要帮助的吗？",
+        },
+    ],
+    usage: {
+        prompt_tokens: 2115,
+        completion_tokens: 253,
+        total_tokens: 2368,
+        prompt_tokens_details: { cached_tokens: 0 },
+        prompt_cache_hit_tokens: 0,
+        prompt_cache_miss_tokens: 2115,
+    },
+    system_fingerprint: "fp_8333852bec_prod0820_fp8_kvcache",
+} satisfies DeepseekTextResponse;
